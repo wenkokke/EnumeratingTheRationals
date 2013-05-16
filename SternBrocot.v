5fe50c4884faa3c97c1ce6d6953d94893c6511d1
Wen Kokke <wenkokke@users.noreply.github.com>
2013-05-16 19:07:33 +0200

    created next function with proofs on the resulting d, l > 0 \/ r > 0 -> d > 0.

--- SternBrocot.v
+++ SternBrocot.v
@@ -1,30 +1,53 @@
+Require Import Coq.Init.Peano.
+Require Import Coq.Arith.Arith.
 
-Module Tree.
+CoInductive Tree (a: Type) :=
+| Node (l: Tree a) (x: a) (r: Tree a).
 
-  CoInductive Tree (A: Type) : Type :=
-  | Leaf : A -> Tree A
-  | Node : Tree A -> A -> Tree A -> Tree A.
+CoFixpoint unfold (A B: Type) (f: B -> (B*A*B)) (x: B) : Tree A :=
+match f x with
+| (l,x,r) => Node A (unfold _ _ f l) x (unfold _ _ f r)
+end.
 
-  CoFixpoint unfold (A B: Type) (f: B -> (B*A*B)) (x: B) : Tree A :=
-    match f x with
-      | (l,a,r) => Node A (unfold _ _ f l) a (unfold _ _ f r)
-    end.
+Inductive rat :=
+| Q (n: nat) (d: nat) (H: d > 0) : rat.
 
-End Tree.
+Notation "n # d" := (Q n d _) (at level 40, left associativity).
+
+Definition next (l r : nat*nat) :=
+match l,r with
+| (l0,l1),(r0,r1) => (l0+r0,l1+r1)
+end.
+
+Lemma next_d : forall l0 l1 r0 r1 n d : nat,
+  (n,d) = next (l0,l1) (r0,r1) -> d = l1 + r1.
+Proof.
+  intros l0 l1 r0 r1.
+  intros n d.
+  intros H.
+  unfold next in H.
+  apply (f_equal (@snd _ _)) in H.
+  simpl in H.
+  assumption.
+Qed.
+
+Theorem next_d_gt_0 : forall l0 l1 r0 r1 n d : nat,
+  ((n,d) = next (l0,l1) (r0,r1)) /\ (l1 > 0 \/ r1 > 0) -> d > 0.
+Proof.
+  intros l0 l1 r0 r1.
+  intros n d.
+  intros H.
+  unfold next in H.
+  decompose [and] H.
+  elim H1.
+  - intros H2.
+    destruct l1 as [|l1].
+    * apply gt_irrefl in H2. inversion H2.
+    * apply next_d in H0. rewrite H0. simpl. apply gt_Sn_O.
+  - intros H2.
+    destruct r1 as [|r1].
+    * apply gt_irrefl in H2. inversion H2.
+    * apply next_d in H0. rewrite H0. rewrite plus_comm. simpl. apply gt_Sn_O.
+Qed.
 
 
-(** The Stern-Brocot Tree *)
-
-Module SternBrocot.
-
-  Theorem adj_ge_l : forall a b c d n m,
-    (a > 0 \/ c > 0) /\ ((n,m) = adj (a,b) (c,d)) -> n > 0.
-  
-  Theorem adj_ge_r : forall a b c d n m,
-    (b > 0 \/ d > 0) /\ ((n,m) = adj (a,b) (c,d)) -> m > 0.
-  
-  Definition adj (l r : N*N)
-  Definition step s
-  Definition tree : Tree Q
-
-End SternBrocot.
