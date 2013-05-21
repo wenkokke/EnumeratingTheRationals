b7e90036cd839096c37cbb4a0df9eadb5845b328
Wen Kokke <wenkokke@users.noreply.github.com>
2013-05-21 16:03:56 +0200

    attempted a definition of bf for trees, and wrote a (probably invalid) definition for bf_acc for cotrees..

--- Trees.v
+++ Trees.v
@@ -1,22 +1,40 @@
-Require Import Streams.
+Inductive tree {A: Type} :=
+  | leaf : tree A
+  | node : tree A -> A -> tree A -> tree A.
 
-Inductive Tree {A: Type} :=
-  | Leaf : Tree A
-  | Node : Tree A -> A -> Tree A -> Tree A.
-
-Fixpoint fold {A B: Type} (f: (B*A*B) -> B) (e: B) (t: Tree A) : B :=
+Fixpoint fold {A B: Type} (f: (B*A*B) -> B) (e: B) (t: tree A) : B :=
   match t with
-  | Leaf       => e
-  | Node l x r => f (fold f e l,x,fold f e r)
+  | leaf       => e
+  | node l x r => f (fold f e l, x, fold f e r)
+  end.
+  
+Fixpoint bf_acc {A: Type} (ts: list (tree A)) : list A :=
+  match ts with
+  | nil       => nil
+  | cons t ts => admit
   end.
 
-CoInductive CoTree {A: Type} :=
-  | CoNode : CoTree A -> A -> CoTree A -> CoTree A.
+CoInductive colist {A: Type} :=
+  | cocons : A -> colist A -> colist A.
 
-CoFixpoint unfold {A B: Type} (f: B -> (B*A*B)) (e: B) : CoTree A :=
+CoInductive cotree {A: Type} :=
+  | conode : cotree A -> A -> cotree A -> cotree A.
+
+CoFixpoint unfold {A B: Type} (f: B -> (B*A*B)) (e: B) : cotree A :=
   match f e with
-  | (l,x,r) => CoNode (unfold f l) x (unfold f r)
+  | (l,x,r) => conode (unfold f l) x (unfold f r)
   end.
- 
+
+Definition bf {A: Type} (t: cotree A) : colist A :=
+  bf_acc [t].
+
+CoFixpoint bf_acc {A: Type} (ts: list (cotree A)) : colist A :=
+  match ts with
+  | nil       => absurd_set
+  | cons t ts =>
+    match t with
+    | conode l x r => cocons x (bf_acc (l :: r :: ts))
+    end
+  end.
 
  
