dfd33b95696c246d4e6297bb644a38073d87a212
Pepijn Kokke <pepijn.kokke@gmail.com>
2013-05-23 20:52:57 +0200

    completed definition of stern_brocot_enum; however, there seems to be something wrong with it, as it's not being evaluated.
    Signed-off-by: Pepijn Kokke <pepijn.kokke@gmail.com>

--- EnumeratingTheRationals.v
+++ EnumeratingTheRationals.v
@@ -56,6 +56,7 @@ Defined.
 
 (** **** CoTrees of Rational Numbers *)
 
+Require Import PArith.
 Require Import NArith.
 Require Import QArith.
 
@@ -68,9 +69,9 @@ Definition calkin_wilf_step (q:positive*positive) : (positive*positive)*Q*(posit
 
 Definition calkin_wilf_tree : cotree Q := cotree_unfold calkin_wilf_step (1,1)%positive.
 
-Definition calkin_wilf_list : colist Q := cotree_bf calkin_wilf_tree.
+Definition calkin_wilf_enum : colist Q := cotree_bf calkin_wilf_tree.
 
-Eval compute in colist_take 10 calkin_wilf_list.
+Eval compute in colist_take 10 calkin_wilf_enum.
 
 (** **** The Stern-Brocot Tree *)
 
@@ -82,9 +83,75 @@ Definition N_pos (n:N) : n<>N0 -> positive.
   exfalso; apply h; reflexivity.
 Defined.
 
-Definition stern_brocot_step (q:(N*N)*(N*N)) : ((N*N)*(N*N))*Q*((N*N)*(N*N)).
-Admitted.
+Definition N_Z (n:N) : Z :=
+  match n with
+    | N0     => Z0 
+    | Npos n => Zpos n
+  end.
 
-Definition stern_brocot_tree : cotree Q := cotree_unfold stern_brocot_step ((0,1),(1,0))%N.
+Lemma Npos_over_Nplus_l : forall n m : N,
+  n <> N0 -> (n + m)%N <> N0.
+Proof.
+  intros n m H.
+  destruct n as [|n].
+  - exfalso; apply H; reflexivity.
+  - destruct m as [|m].
+    * simpl; assumption.
+    * simpl; discriminate.
+Qed.
 
-Definition stern_brocot_list : colist Q := cotree_bf stern_brocot_tree.
+Lemma Npos_over_Nplus_r : forall n m : N,
+  m <> N0 -> (n + m)%N <> N0.
+Proof.
+  intros n m.
+  rewrite Nplus_comm.
+  apply Npos_over_Nplus_l.
+Qed.
+
+Lemma Npos_over_Nplus : forall n m : N,
+  (n <> N0 \/ m <> N0) -> (n + m)%N <> N0.
+Proof.
+  intros n m H.
+  elim H.
+  - apply Npos_over_Nplus_l.
+  - apply Npos_over_Nplus_r.
+Qed.
+
+Inductive stern_brocot_pair : Type :=
+| Qpair (lm ln rm rn : N) (HL: lm<>N0 \/ rm<>N0) (HR: ln<>N0 \/ rn<>N0) : stern_brocot_pair.
+
+Definition stern_brocot_step (q:stern_brocot_pair) : stern_brocot_pair*Q*stern_brocot_pair.
+  refine (match q with
+            | Qpair a b c d HL HR =>
+              let ac := (a + c)%N in
+              let bd := (b + d)%N in
+              let l  := Qpair a  b  ac bd _ _ in
+              let r  := Qpair ac bd  c  d _ _ in
+              let q  := Qmake (N_Z ac) (N_pos bd _) in
+              (l,q,r)
+          end).
+  - right; unfold ac; apply Npos_over_Nplus in HL; assumption.
+  - right; unfold bd; apply Npos_over_Nplus in HR; assumption.
+  - left ; unfold ac; apply Npos_over_Nplus in HL; assumption.
+  - left ; unfold bd; apply Npos_over_Nplus in HR; assumption.
+  - unfold bd; apply Npos_over_Nplus in HR; assumption.
+Qed.
+
+Definition stern_brocot_tree : cotree Q.
+  refine (cotree_unfold stern_brocot_step (Qpair 0 1 1 0 _ _)%N).
+  - right; discriminate.
+  - left ; discriminate.
+Qed.
+
+Definition stern_brocot_enum : colist Q := cotree_bf stern_brocot_tree.
+
+(** there's something funny going on here *)
+
+Definition test_stern_brocot_step1 : stern_brocot_pair*Q*stern_brocot_pair.
+Proof.
+  refine (stern_brocot_step (Qpair 0 1 1 0 _ _)%N).
+  - right; discriminate.
+  - left ; discriminate.
+Defined.
+
+Eval compute in test_stern_brocot_step1.
