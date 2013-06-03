88e1b0133e33e9d1ee00e751f1745447351bf12f
Wen Kokke <wenkokke@users.noreply.github.com>
2013-06-03 02:26:52 +0200

    improved definition of "in" for colists, but current version is broken (didn't update definitions for cotrees).
    Signed-off-by: Pepijn Kokke <wen.kokke@gmail.com>

--- EnumeratingTheRationals.v
+++ EnumeratingTheRationals.v
@@ -50,30 +50,70 @@ Module CoList.
         end
     end.
 
-  Inductive In {A: Type} (P: A -> Prop) (l: colist A) : Prop :=
-  | At (n: nat) : P (lookup n l) -> In P l.
-
-  Theorem In_Exists : forall {A: Type} (P: A -> Prop) (l: colist A),
-    In P l -> Exists (fun l => P (hd l)) l.
-  Proof.
-    intros A P l H; elim H; clear H; intro n.
-    generalize l; clear l.
-    induction n as [|n].
-    - intros l H; destruct l as [e l].
-      apply Exists_here; simpl in *; assumption.
-    - intros l H; destruct l as [e l].
-      apply IHn in H; apply Exists_further; simpl; assumption.
-  Qed.
+  Inductive In {A: Type} (x: A) (xs: colist A) : Prop :=
+  | In_here    : (x = hd xs) -> In x xs
+  | In_further : In x (tl xs) -> In x xs.
   
-  Theorem Exists_In : forall {A: Type} (P: A -> Prop) (l: colist A),
-    Exists (fun l => P (hd l)) l -> In P l.
+  Definition At : forall {A: Type} (x: A) (xs: colist A) (n: nat), (x = lookup n xs) -> In x xs.
   Proof.
-    intros A P l H; elim H.
-    - clear H l; intros l H.
-      apply (At P l 0); simpl; assumption.
-    - clear H l; intros l H IH; elim IH; clear IH; intros n IH.
-      apply (At P l (S n)).
-      destruct l as [e l]; simpl in *; assumption.
+    intros A x xs n; generalize xs; clear xs.
+    induction n as [|n].
+    - apply In_here.
+    - intro xs; destruct xs as [y ys]; unfold lookup in *; simpl in *; intro H.
+      apply IHn in H; apply In_further; simpl; assumption.
+  Defined.
+
+  Theorem In_At : forall {A: Type} (x: A) (xs: colist A),
+    In x xs -> exists n, x = lookup n xs.
+  Proof.
+    intros A x xs H; elim H; clear H xs.
+    - intros xs H; exists 0%nat; destruct xs. unfold lookup. simpl in *; assumption.
+    - intros xs H IH; elim IH; clear IH; intros n IH; exists (S n).
+      unfold lookup in *; simpl in *; assumption.
+  Qed.
+
+  Definition At_Exists : forall {A: Type} (P: A -> Prop) (xs: colist A) (n: nat),
+    P (lookup n xs) -> Exists (fun l => P (hd l)) xs.
+  Proof.
+    intros A P xs n; generalize xs; clear xs.
+    induction n as [|n].
+    - intros xs H; unfold lookup in H; simpl in H.
+      apply Exists_here; assumption.
+    - intros xs H; unfold lookup in *; simpl in *.
+      apply IHn in H; apply Exists_further; assumption.
+  Defined.
+      
+  Theorem In_Exists : forall {A: Type} (P: A -> Prop) (x: A) (xs: colist A),
+    P x /\ In x xs -> Exists (fun l => P (hd l)) xs.
+  Proof.
+    intros A P x xs H; elim H; clear H; intros HP HM.
+    apply In_At in HM; elim HM; clear HM; intros n HM.
+    apply (At_Exists P xs n); rewrite HM in HP; assumption.
+  Qed.
+
+  Lemma Forall_here : forall {A: Type} (P: A -> Prop) xs,
+    Forall (fun l => P (hd l)) xs -> P (hd xs).
+  Proof.
+  Admitted.
+
+  Lemma Forall_further : forall {A: Type} (P: colist A -> Prop) xs,
+    Forall P xs -> Forall P (tl xs).
+  Proof.
+  Admitted.
+
+  Theorem Forall_In : forall {A: Type} (P: A -> Prop) xs,
+    Forall (fun l => P (hd l)) xs -> forall x, In x xs -> P x.
+  Proof.
+    intros A P xs Hall x Hin.
+    apply In_At in Hin; elim Hin; intros n Heq.
+    generalize Hall Heq; clear Hall Hin Heq.
+    generalize xs; clear xs.
+    induction n as [|n].
+    - intros xs Hall Heq.
+      destruct xs as [y ys]; unfold lookup in Heq; simpl in Heq.
+      apply Forall_here in Hall; simpl in Hall; rewrite Heq; assumption.
+    - intros xs Hall Heq.
+      apply Forall_further in Hall; apply IHn in Hall; assumption.    
   Qed.
   
 End CoList.
@@ -241,9 +281,11 @@ Module CoTree.
   Qed.
 
   (** [In] defines proofs on the elements of a [cotree]. *)
-
-  Inductive In {A: Type} (P: A -> Prop) (t: cotree A) : Prop :=
-  | At (p: path) : P (lookup p t) -> In P t.
+  
+  Inductive In {A: Type} (x: A) (t: cotree A) : Prop :=
+  | In_root  : (x = root t) -> In x t.
+  | In_left  : In x (left t) -> In x t.
+  | In_right : In x (right t) -> In x t.
 
   (** [Exists] defines proofs on the subtrees of a [cotree] *)
 
